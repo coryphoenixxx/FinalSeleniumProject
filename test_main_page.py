9ce80a531b937c6c878bb1e4dd830b9e8f33439a
coryphoenixxx <coryphoenixxx@gmail.com>
2021-01-26 15:49:44 +0700

    Add exception-handler

--- test_main_page.py
+++ test_main_page.py
@@ -6,8 +6,6 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
 
 
 def test_guest_should_see_login_link(browser):
