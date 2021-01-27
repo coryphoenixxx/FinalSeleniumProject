877b0d5a444cb0b13eccc241d8055224afd70ec0
coryphoenixxx <coryphoenixxx@gmail.com>
2021-01-27 07:55:22 +0700

    Add tests for promos

--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,10 @@
+import pytest
 from .pages.product_page import ProductPage
 
-link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
 
+main_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/" #?promo=newYear2019"
 
+@pytest.mark.parametrize('link', [f"{main_link}?promo=offer{i}" for i in range(10)])
 def test_guest_can_add_product_to_basket(browser):
     page = ProductPage(browser, link)
     page.open()
